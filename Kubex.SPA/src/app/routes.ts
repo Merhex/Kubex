289665eacbf820fa38a44f95ae10d325a53c510a
Kurt Bellens <kurt.bellens@outlook.be>
2020-04-02 22:19:56 +0200

    Restart

---
+++ Kubex.SPA/src/app/routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+//import { HomeComponent } from './home/home.component';
+//import { ClientComponent } from './client/client.component';
+
+export const appRoutes: Routes = [
+    //{ path: 'home', component: HomeComponent },
+    //{ path: 'clients', component: ClientComponent },
+    { path: '**', redirectTo: 'home', pathMatch: 'full' }
+];
