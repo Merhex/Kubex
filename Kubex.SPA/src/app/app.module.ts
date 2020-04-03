cbe39d72c305f6bd576a4480909d4000bc2b57ca
Kurt Bellens <kurt.bellens@outlook.be>
2020-04-03 22:50:43 +0200

    Baby steps into Frontend

--- Kubex.SPA/src/app/app.module.ts
+++ Kubex.SPA/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      NavComponent
+      NavComponent,
+      UserComponent,
+      HomeComponent
    ],
    imports: [
       BrowserModule,
--- Kubex.SPA/src/app/routes.ts
+++ Kubex.SPA/src/app/routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-//import { HomeComponent } from './home/home.component';
-//import { ClientComponent } from './client/client.component';
+import { HomeComponent } from './home/home.component';
+// import { ClientComponent } from './client/client.component';
 
 export const appRoutes: Routes = [
-    //{ path: 'home', component: HomeComponent },
-    //{ path: 'clients', component: ClientComponent },
+    { path: 'home', component: HomeComponent },
+    // { path: 'clients', component: ClientComponent },
     { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
