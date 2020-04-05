93500481357015dd9db88a19576d8f495c87374b
Kurt Bellens <kurt.bellens@outlook.be>
2020-04-05 19:39:30 +0200

    Added JSON Server as mock REST API

--- Kubex.SPA/src/app/routes.ts
+++ Kubex.SPA/src/app/routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
 
 export const appRoutes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'users', component: UserComponent },
     { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
