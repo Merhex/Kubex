11342cb4b5f3cca870510a0804fa41fd541255dd
Kurt Bellens <kurt.bellens@outlook.be>
2020-04-26 12:59:33 +0200

    DAR improved with Angular Flex

---
+++ Kubex.SPA/src/app/dailyactivityreport/subentry/subentry.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subentry',
+  templateUrl: './subentry.component.html',
+  styleUrls: ['./subentry.component.css']
+})
+export class SubentryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
