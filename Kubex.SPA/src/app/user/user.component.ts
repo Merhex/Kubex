80e429a9f19e7ceafbd5045ee194f4a45c016b58
Kurt Bellens <kurt.bellens@outlook.be>
2020-05-07 21:05:56 +0200

    Making ready for DTO

--- Kubex.SPA/src/app/user/user.component.ts
+++ Kubex.SPA/src/app/user/user.component.ts
@@ -1,3 +1,5 @@
+import { User } from './../Models/user';
+import { UserService } from './../Service/user.service';
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
@@ -10,19 +12,23 @@ import { HttpClient } from '@angular/common/http';
 export class UserComponent implements OnInit {
   // @Input() inputUser: UserlistComponent;
   // @Output() outputUser = new EventEmitter<any>();
-  selectedUser = null;
+  selectedUser: User = null;
 
-  users: Observable<any> = this.http.get('http://localhost:3000/users/');
+  // users: Observable<any> = this.http.get('http://localhost:3000/users/');
   // users: Observable<any> = this.http.get('api/users');
+  users: User[];
 
   displayUser(userId) {
-    return this.http.get('http://localhost:3000/users/' + userId).subscribe(response => {
-      // this.outputUser.emit(response);
-      this.selectedUser = response;
+    this.userService.getUser(userId).subscribe(user => {
+      this.selectedUser = user;
     });
   }
 
-  constructor(private http: HttpClient) {}
+  constructor(private userService: UserService) {
+    userService.getAllUsers().subscribe(users => {
+      this.users = users;
+    });
+  }
 
   ngOnInit() {}
 
