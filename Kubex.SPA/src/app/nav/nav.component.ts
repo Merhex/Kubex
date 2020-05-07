94a3ff1786167699aaee9f432782e13ec99e2399
Kurt Bellens <kurt.bellens@outlook.be>
2020-05-07 22:08:58 +0200

    Started on authentication

--- Kubex.SPA/src/app/nav/nav.component.ts
+++ Kubex.SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../_services/index.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  currentUser: any;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+  }
 
   ngOnInit() {
   }
 
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
 }
--- Kubex.SPA/src/app/routes.ts
+++ Kubex.SPA/src/app/routes.ts
@@ -3,11 +3,14 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { UserComponent } from './user/user.component';
 import { DailyactivityreportComponent } from './dailyactivityreport/dailyactivityreport.component';
+import { AuthGuard } from './_helpers';
 
 export const appRoutes: Routes = [
-    { path: 'home', component: HomeComponent },
+    { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
     { path: 'users', component: UserComponent },
     { path: 'dar', component: DailyactivityreportComponent },
+
+    // All other requests needs to be redirected to home
     { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
