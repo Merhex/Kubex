d159c63956c5cbfd34b9bf0df0dccd543cb909ac
Kurt Bellens <kurt.bellens@outlook.be>
2020-08-19 20:36:44 +0200

    delete/update post + small dar ui change

--- Kubex.SPA/src/app/_services/post.service.ts
+++ Kubex.SPA/src/app/_services/post.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { AccountService } from './account.service';
 import { HttpClient } from '@angular/common/http';
 import { Post } from '../_models';
 import { Observable } from 'rxjs';
@@ -12,15 +11,14 @@ export class PostService {
 
     baseUrl = environment.apiUrl + '/post/';
 
-    constructor(private accountService: AccountService,
-                private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
 
-    create(params: Post) {
-        return this.http.post<Post>(this.baseUrl + 'create/', params);
+    create(post: Post) {
+        return this.http.post<Post>(this.baseUrl + 'create/', post);
     }
 
-    update() {
-        // return this.http.patch<Post>(this.baseUrl + `update/${id}`, params);
+    update(post: Post) {
+        return this.http.patch<Post>(this.baseUrl + `update/${post.id}`, post);
     }
 
     get(id: number) {
--- Kubex.SPA/src/app/shared/posts/posts.component.ts
+++ Kubex.SPA/src/app/shared/posts/posts.component.ts
@@ -33,11 +33,11 @@ export class PostsComponent implements OnInit {
   openDialog(post?: Post) {
     let newPost: Post;
 
-    if (post === null) {
+    if (post) {
+      newPost = post;
+    } else {
       newPost = new PostCreate();
       newPost.address = this.company.address;
-    } else {
-      newPost = post;
     }
 
     newPost.companyId = this.company.id;
@@ -49,34 +49,68 @@ export class PostsComponent implements OnInit {
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      this.postService.create(result)
-                      .pipe(first())
-                      .subscribe(
-                        (data) => {
-                          let userToUpdate: User;
+      if (post === null) {
+        this.postService.create(result)
+          .pipe(first())
+          .subscribe(
+            (data) => {
+              let userToUpdate: User;
 
-                          this.accountService.user.subscribe(user => {
-                            userToUpdate = user;
-                          });
+              this.accountService.user.subscribe(user => {
+                userToUpdate = user;
+              });
 
-                          userToUpdate.postIds.push(data.id);
+              userToUpdate.postIds.push(data.id);
 
-                          this.accountService.updateUser(userToUpdate);
-                          console.log(userToUpdate);
+              this.accountService.updateUser(userToUpdate);
+              console.log(userToUpdate);
 
-                          this.company.posts.push(result);
-                          this.companyChange.emit(result);
+              this.company.posts.push(result);
+              this.companyChange.emit(result);
 
-                          this.alertService.success('Post successfully registered', { keepAfterRouteChange: true });
-                        },
-                        (err) => {
-                            this.alertService.error(err);
-                        });
+              this.alertService.success('Post successfully registered', { keepAfterRouteChange: true });
+            },
+            (err) => {
+                this.alertService.error(err);
+            });
+      } else {
+        this.postService.update(result)
+          .pipe(first())
+          .subscribe(
+            (data) => {
+              let userToUpdate: User;
+
+              this.accountService.user.subscribe(user => {
+                userToUpdate = user;
+              });
+
+              userToUpdate.postIds.push(data.id);
+
+              this.accountService.updateUser(userToUpdate);
+              console.log(userToUpdate);
+
+              // verwijder de bestaande post van de company
+              const index = this.company.posts.indexOf(post);
+              this.company.posts.splice(index, 1);
+
+              // voeg de geupdate post terug toe
+              this.company.posts.push(result);
+              this.companyChange.emit(result);
+
+              this.alertService.success('Post successfully updated', { keepAfterRouteChange: true });
+            },
+            (err) => {
+              this.alertService.error(err);
+            });
+      }
     });
   }
 
   deletePost(post: Post) {
-    if (post.id == null) {
+    if (post.id === null) {
+      const index = this.company.posts.indexOf(post);
+      this.company.posts.splice(index, 1);
+    } else {
       this.postService.delete(post.id)
       .pipe(first())
         .subscribe(() => {
@@ -85,9 +119,6 @@ export class PostsComponent implements OnInit {
         error => {
             this.alertService.error(error);
         });
-    } else {
-      const index = this.company.posts.indexOf(post);
-      this.company.posts.splice(index, 1);
     }
   }
 }
